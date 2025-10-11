df7c4856f30f83e249f791c971d4b63591d1abb9
Xiao-yuki <victor970115@gmail.com>
2025-10-11 16:50:47 +0800

    一點進度

--- text-generation-webui/extensions/memory_v1_2/keyword_search.py
+++ text-generation-webui/extensions/memory_v1_2/keyword_search.py
@@ -1,5 +1,11 @@
 
+last_keyword = "none"#初始化
+
 class keyword_search_system:
     def keyword_search(string):
         keyword = "none"#暫時 之後補上提取模型
+        if last_keyword == keyword:
+            keyword = "none"
+        else:
+            last_keyword = keyword
         return keyword
